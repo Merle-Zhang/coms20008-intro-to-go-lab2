6696154de1fd4b97ebdf4cd6e2ac53d6ee5fc94c
Merle@wsl.surface <merle.xzhang@gmail.com>
2020-10-12 15:57:15 +0100

    Q2

--- ping/ping.go
+++ ping/ping.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"runtime/trace"
@@ -9,17 +10,36 @@ import (
 
 func foo(channel chan string) {
 	// TODO: Write an infinite loop of sending "pings" and receiving "pongs"
-
+	message := ""
+	for true {
+		message = "ping"
+		channel <- message
+		fmt.Println("Foo is sending:", message)
+		// time.Sleep(500 * time.Millisecond) // TODO: why don't need this line?
+		// Ans: If the channel isn't buffered, the program will sleep when it sends until the channel is read from, so it isn't possible to receive what you just sent
+		message = <-channel
+		fmt.Println("Foo has received:", message)
+		fmt.Println()
+	}
 }
 
 func bar(channel chan string) {
 	// TODO: Write an infinite loop of receiving "pings" and sending "pongs"
+	message := ""
+	for true {
+		message = <-channel
+		fmt.Println("Bar has received:", message)
+		message = "pong"
+		channel <- message
+		fmt.Println("Bar is sending:", message)
+	}
 }
 
 func pingPong() {
 	// TODO: make channel of type string and pass it to foo and bar
-	go foo(nil) // Nil is similar to null. Sending or receiving from a nil chan blocks forever.
-	go bar(nil)
+	channel := make(chan string)
+	go foo(channel) // Nil is similar to null. Sending or receiving from a nil chan blocks forever.
+	go bar(channel)
 	time.Sleep(500 * time.Millisecond)
 }
 
